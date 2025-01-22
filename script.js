af0a8098769340a65041e00f95d62a4c384a36dc
Mwiti Peter <mwitipet@gmail.com>
2025-01-22 19:21:40 +0300

    Advancejsfirstcommit

---
+++ script.js
@@ -0,0 +1,12 @@
+// console.log("script hello")
+// console.error("error")
+// console.warn("warning")
+
+// const V=64
+// console.log (`V=${V}`)
+
+// const prompt = require("prompt-sync")()
+// const V = prompt("type something ")
+
+// console.log(V)
+
